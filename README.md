6ad282cb0b0219a86ffedf0dbab3fbe6cf051cdd
michaelsynan <93357198+michaelsynan@users.noreply.github.com>
2023-03-08 21:40:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nuxt-Tailwind
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-v8zsdv)
---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
