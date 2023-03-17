ea98367865dd0f5bcd2614832c37944b451e2d96
mikey <mforest@protonmail.com>
2023-03-16 21:33:03 -0400

    Added base styles and default layout

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    <NuxtPage />
+    <NuxtLayout />
   </div>
 </template>
+
+
+<script>
+
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Layout: default
+    <NuxtPage />
+  </div>
+</template>
+
+<style scoped></style>
