f790c481cc6bba3605a6d2be2f421eae667a575f
mikey <mforest@protonmail.com>
2023-05-08 07:32:27 -0400

    removed app.vue, updated default.vue layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,25 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import '@/assets/css/main.css'
+
+</script>
 
 <template>
   <div>
+    <navbar />
     <slot />
   </div>
 </template>
 
-<style scoped></style>
+
+<style scoped>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.15s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(.25rem);
+}
+
+</style>
